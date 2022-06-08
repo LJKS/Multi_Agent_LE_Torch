e6b23631e006f94002768b34521ffebb9d39e90a
Leon Schmid <lschmid@uos.de>
2022-06-08 22:50:26 +0200

    fundamental stuff is running, now refactor and include better options for meta-optimization

--- bash_scripts/run_tscl_population_training_lstm128.sh
+++ bash_scripts/run_tscl_population_training_lstm128.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=30
 #SBATCH --partition=gpu
-
+source $HOME/.bashrc
 spack load miniconda3@4.10.3
 conda activate Multi_Agent_LE_Torch2
 python $HOME/experiments/Multi_Agent_LE_Torch/scripts.py --experiment tscl_population_training_lstm128 --batch_size 4096
