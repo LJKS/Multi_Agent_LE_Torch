ca13b3177642cd9d548048a9e5aa2ede673d1bbb
Leon Schmid <lschmid@uos.de>
2022-09-19 11:18:41 +0200

    rewrite some run scripts - fix script arg error

--- bash_scripts/continue_baseline_small.sh
+++ bash_scripts/continue_baseline_small.sh
@@ -10,7 +10,7 @@ source $HOME/.bashrc
 spack load miniconda3@4.10.3
 conda activate Multi_Agent_LE_Torch2
 #run 50 epochs per step
-for i in {1..$2}
+for (( i=1; i<=$2; i++ ))
 do
   srun python $HOME/experiments/Multi_Agent_LE_Torch/scripts.py --experiment baseline_population_training --num_senders 2 --num_receivers 2 --finetuning_epochs 50 --save_every 1 --run_key $1 --extend True
   #just make sure system got time to save all the models and stuff...
