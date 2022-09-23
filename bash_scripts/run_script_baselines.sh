b2cbb4a63e8a97ec874146734757d0a84e13a0cd
Leon Schmid <lschmid@uos.de>
2022-09-23 10:16:57 +0200

    rewrite some run scripts - fix script arg error, reset commentary code to old for future fix, add new runs for tscl

--- bash_scripts/run_script_baselines.sh
+++ bash_scripts/run_script_baselines.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # submission scripts are structured as  | script_name | iterations | run_name |
 # notice: each iteration is 50 epochs (+ one additional starting iteration of also 50 epochs), run_names make it easy to track experiments by name
-sbatch start_baseline_small.sh 9 kermes_whale
-sbatch start_baseline_small.sh 9 pyrrhous_duck
-sbatch start_baseline_small.sh 9 nigrine_wolf
-sbatch start_baseline_medium.sh 49 titian_sparrow
-sbatch start_baseline_medium.sh 49 brunneous_shrimp
-sbatch start_baseline_medium.sh 49 rhodopsin_turtle
+sbatch start_baseline_small.sh kermes_whale 9
+sbatch start_baseline_small.sh pyrrhous_duck 9
+sbatch start_baseline_small.sh nigrine_wolf 9
+sbatch start_baseline_medium.sh titian_sparrow 49
+sbatch start_baseline_medium.sh brunneous_shrimp 49
+sbatch start_baseline_medium.sh rhodopsin_turtle 49
 # these might be too short, but I do not want to create jobs that are longer than 4 days really.
-sbatch start_baseline_large.sh 49 meline_monkey
-sbatch start_baseline_large.sh 49 taupe_ostrich
-sbatch start_baseline_large.sh 49 miniaceous_kangaroo
+sbatch start_baseline_large.sh meline_monkey 49
+sbatch start_baseline_large.sh taupe_ostrich 49
+sbatch start_baseline_large.sh miniaceous_kangaroo 49
