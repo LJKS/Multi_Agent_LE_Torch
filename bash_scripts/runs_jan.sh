17249d1aa77c9ec572663a0704b16604a8bc5d34
Leon Schmid <lschmid@uos.de>
2023-01-05 19:16:49 +0100

    rewrite iteratively deployed runscript, nice (low priority submissions) and submit plan for january

--- bash_scripts/runs_jan.sh
+++ bash_scripts/runs_jan.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# 2x2 baseline runs, 200 epochs each
+# 2x2 tscl runs, 200 epochs each
 bash iterated_deployment.sh -e tscl_population_training -n salty_lightning -a 2 -i 4 -s true
 bash iterated_deployment.sh -e tscl_population_training -n dazzling_hawk -a 2 -i 4 -s true
-# 5x5 baseline runs, 1000 epochs each
+# 5x5 tscl runs, 1000 epochs each
 bash iterated_deployment.sh -e tscl_population_training -n basic_behemoth -a 5 -i 20 -s true
 bash iterated_deployment.sh -e tscl_population_training -n amazing_guardian -a 5 -i 20 -s true
-# 10x10 baseline runs, 2500 epochs each
+# 10x10 tscl runs, 2500 epochs each
 bash iterated_deployment.sh -e tscl_population_training -n hard_rose -a 10 -i 100 -s true
 bash iterated_deployment.sh -e tscl_population_training -n impure_cayman -a 10 -i 100 -s true
-# 15x15 baseline runs, 5000 epochs each
+# 15x15 tscl runs, 5000 epochs each
 bash iterated_deployment.sh -e tscl_population_training -n agile_neptune -a 15 -i 200 -s true
 bash iterated_deployment.sh -e tscl_population_training -n reckless_ghost -a 15 -i 200 -s true
 
