fb040e82b8a70b7b6e0a0161986138ec2e7ee6c4
Leon Schmid <lschmid@uos.de>
2023-01-05 18:20:57 +0100

    rewrite iteratively deployed runscript

--- bash_scripts/iterated_deployment.sh
+++ bash_scripts/iterated_deployment.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # args: $1 --> run_key (name), $2 num_agents (int), $3 how many iterations beyond the first should be deployed
 JOBID=$(sbatch runscript.sh $1 $2 False --parsable)
-
+sleep 1
 for (( i=1; i<=$3; i++ ))
 do
   JOBID=$(sbatch --parsable --dependency=afterok:${JOBID##* }} runscript.sh $1 $2 True)
   echo $JOBID
+  sleep 1
 done
